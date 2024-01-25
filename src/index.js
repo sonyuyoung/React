3111cae4c7cda72504a538dd98d13a42d338b570
02 <sonyuyoung@naver.com>
2024-01-26 00:06:27 +0900

    1차 _ yarn start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// public -> index.html -> root
+// ReactDOM 현재문서에 root 라는 아이디를 가져온디
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// 그 루트에 render 그려준다
+root.render(<App />);
